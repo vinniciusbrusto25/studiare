794613590383962761ef5963d2ef4ef945d782ec
Vinicius Brustolin <vinnicius_brusto@hotmail.com>
2023-01-30 21:49:12 -0300

    Adding files

---
+++ backend/src/main/java/com/bruv/studymonitorapi/StudymonitorApiApplication.java
@@ -0,0 +1,13 @@
+package com.bruv.studymonitorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudymonitorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudymonitorApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bruv/studymonitorapi/StudymonitorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruv.studymonitorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudymonitorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
